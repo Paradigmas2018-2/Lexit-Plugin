4e7a2a948aa021adfcb9873eed71dfb54c11906f
Arthur Diniz <arthurbdiniz@gmail.com>
2018-09-09 20:48:23 -0300

    Change change forms to a work formater
    Signed-off-by: Arthur Diniz <arthurbdiniz@gmail.com>

--- src/config/chrome/popup_files/popup.js
+++ src/config/chrome/popup_files/popup.js
@@ -1,98 +1,102 @@
 'use strict';
 
 // let changeColor = document.getElementById('changeColor');
-let buttonScenario = document.getElementById('buttonScenario');
-let buttonLexicon = document.getElementById('buttonLexicon');
-let buttonAddSinonimo = document.getElementById('addSinonimo');
-let buttonDelSinonimo = document.getElementById('delSinonimo');
-let buttonSubmitLexicon = document.getElementById('submitLexicon');
+let buttonFormact = document.getElementById('formact');
 
-buttonScenario.onclick = function(element) {
-  document.getElementById("scenario").style.display = "block";
-  document.getElementById("lexicon").style.display = "none";
-}
+function post(path, params, method) {
+  method = method || "post"; // Set method to post by default if not specified.
 
-buttonLexicon.onclick = function(element) {
-  document.getElementById("lexicon").style.display = "block";
-  document.getElementById("scenario").style.display = "none";
-}
+  // The rest of this code assumes you are not using a library.
+  // It can be made less wordy if you use one.
+  var form = document.createElement("form");
+  form.setAttribute("method", method);
+  form.setAttribute("action", path);
 
-buttonAddSinonimo.onclick = function(element) {
-  addSinonimo()
-}
+  for(var key in params) {
+      if(params.hasOwnProperty(key)) {
+          var hiddenField = document.createElement("input");
+          hiddenField.setAttribute("type", "hidden");
+          hiddenField.setAttribute("name", key);
+          hiddenField.setAttribute("value", params[key]);
 
-buttonDelSinonimo.onclick = function(element) {
-  delSinonimo()
-}
-
-// buttonSubmitLexicon.onclick = function(element) {
-//   TestarBranco()
-// }
-
-function TestarBranco() {
-
-  var form = document.forms[1];
-  var nome  = form.nome.value;
-  var nocao = form.nocao.value;
-
-  if( nome == "" )
-    {
-	  alert (" Por favor, forneça o NOME do léxico.\n O campo NOME é de preenchimento obrigatório.");
-      form.nome.focus();
-      return false;
-    }else{
-		padrao = /[\\\/\?"<>:|]/;
-		var nOK = padrao.exec(nome);
-		if (nOK)
-		{
-			window.alert ("O nome do léxico não pode conter nenhum dos seguintes caracteres:   / \\ : ? \" < > |");
-			form.nome.focus();
-			return false;
-		}
-	}
-
-   if( nocao == "" )
-    { alert (" Por favor, forneça a NOÇÂO do léxico.\n O campo NOÇÂO É de preenchimento obrigatório.");
-      form.nocao.focus();
-      return false;
-    }
-
-}
-
-function addSinonimo() {
-  var listSinonimo = document.forms[1].elements['listSinonimo[]'];
-
-  if(document.forms[1].sinonimo.value == "")
-  	return;
-
-  var sinonimo = document.forms[1].sinonimo.value;
-  var padrao = /[\\\/\?"<>:|]/;
-  var nOK = padrao.exec(sinonimo);
-  if (nOK) {
-  	window.alert ("O sin�nimo do l�xico n�o pode conter nenhum dos seguintes caracteres:   / \\ : ? \" < > |");
-  	document.forms[1].sinonimo.focus();
-  	return;
+          form.appendChild(hiddenField);
+      }
   }
 
-  listSinonimo.options[listSinonimo.length] = new Option(document.forms[1].sinonimo.value, document.forms[1].sinonimo.value);
-
-  document.forms[1].sinonimo.value = "";
-
-  document.forms[1].sinonimo.focus();
-
+  document.getElementById('scenario').appendChild(form);
+  console.log('teste')
+  form.submit();
 }
 
-function delSinonimo() {
-  var listSinonimo = document.forms[1].elements['listSinonimo[]'];
-
-  if(listSinonimo.selectedIndex == -1)
-    return;
-  else
-
-  listSinonimo.options[listSinonimo.selectedIndex] = null;
-
-  delSinonimo();
+buttonSubmitLexicon.onclick = function(element) {
+  console.log('teste')
+  post('/contact/', {name: 'Johnny Bravo'});
 }
+
+// function TestarBranco() {
+
+//   var form = document.forms[1];
+//   var nome  = form.nome.value;
+//   var nocao = form.nocao.value;
+
+//   if( nome == "" )
+//     {
+// 	  alert (" Por favor, forneça o NOME do léxico.\n O campo NOME é de preenchimento obrigatório.");
+//       form.nome.focus();
+//       return false;
+//     }else{
+// 		padrao = /[\\\/\?"<>:|]/;
+// 		var nOK = padrao.exec(nome);
+// 		if (nOK)
+// 		{
+// 			window.alert ("O nome do léxico não pode conter nenhum dos seguintes caracteres:   / \\ : ? \" < > |");
+// 			form.nome.focus();
+// 			return false;
+// 		}
+// 	}
+
+//    if( nocao == "" )
+//     { alert (" Por favor, forneça a NOÇÂO do léxico.\n O campo NOÇÂO É de preenchimento obrigatório.");
+//       form.nocao.focus();
+//       return false;
+//     }
+
+// }
+
+// function addSinonimo() {
+//   var listSinonimo = document.forms[1].elements['listSinonimo[]'];
+
+//   if(document.forms[1].sinonimo.value == "")
+//   	return;
+
+//   var sinonimo = document.forms[1].sinonimo.value;
+//   var padrao = /[\\\/\?"<>:|]/;
+//   var nOK = padrao.exec(sinonimo);
+//   if (nOK) {
+//   	window.alert ("O sin�nimo do l�xico n�o pode conter nenhum dos seguintes caracteres:   / \\ : ? \" < > |");
+//   	document.forms[1].sinonimo.focus();
+//   	return;
+//   }
+
+//   listSinonimo.options[listSinonimo.length] = new Option(document.forms[1].sinonimo.value, document.forms[1].sinonimo.value);
+
+//   document.forms[1].sinonimo.value = "";
+
+//   document.forms[1].sinonimo.focus();
+
+// }
+
+// function delSinonimo() {
+//   var listSinonimo = document.forms[1].elements['listSinonimo[]'];
+
+//   if(listSinonimo.selectedIndex == -1)
+//     return;
+//   else
+
+//   listSinonimo.options[listSinonimo.selectedIndex] = null;
+
+//   delSinonimo();
+// }
 
 
 // chrome.storage.sync.get('color', function(data) {
